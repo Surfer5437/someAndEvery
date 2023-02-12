f0835497a0e4cd3655f0d9d04b553d6d7caf22b2
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-02-12 17:06:07 -0500

    finished exercise

--- app.js
+++ app.js
@@ -49,17 +49,18 @@ function hasOnlyOddNumbers(arr) {
 // Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
 // hasNoDuplicates([1,2,3,1]) // false
 // hasNoDuplicates([1,2,3]) // true
-function hasNoDuplicates(nums) {
-    return nums.every(function (num) {
-        return nums.indexOf(num) === nums.lastIndexOf(num);
+function hasNoDuplicates(arr) {
+    return arr.every(function (val) {
+        return arr.indexOf(val) === arr.lastIndexOf(val);
     });
-};
-
+}
 
 
 
 // hasCertainKey
-// Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
+// Write a function called hasCertainKey which accepts an array of objects
+//  and a key, and returns true if every single object in the array contains that key.
+//  Otherwise it should return false.
 // let arr = [
 //     {title: "Instructor", first: 'Elie', last:"Schoppik"},
 //     {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true},
@@ -68,7 +69,11 @@ function hasNoDuplicates(nums) {
 //   ]
 //   hasCertainKey(arr,'first') // true
 //   hasCertainKey(arr,'isCatOwner') // false
-
+function hasCertainKey(arr, key) {
+    return arr.every(function (val) {
+        return key in val
+    })
+}
 
 
 
@@ -83,3 +88,8 @@ function hasNoDuplicates(nums) {
 //   ]
 //   hasCertainValue(arr,'title','Instructor') // true
 //   hasCertainValue(arr,'first','Elie') // false
+function hasCertainValue(arr, key, searchValue) {
+    return arr.every(function (val) {
+        return val[key] === searchValue;
+    })
+}
