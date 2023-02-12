c3848815786be821552f50820eea05b0bf1387d0
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-02-12 13:58:13 -0500

    added hasOnlyOddNumbers function

--- app.js
+++ app.js
@@ -31,7 +31,12 @@ function hasAZero(numbers) {
 // Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false.
 // hasOnlyOddNumbers([1,3,5,7]) // true
 // hasOnlyOddNumbers([1,2,3,5,7]) // false
-
+function hasOnlyOddNumbers(arr) {
+    const oddNum = arr.every(num => {
+        return num % 2 === 1;
+    });
+    return oddNum;
+};
 
 
 
